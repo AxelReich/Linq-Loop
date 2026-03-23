dc4b6f19c5efe4db28316fb8f25241d825dff124
Axel <axreich@gmail.com>
2026-03-23 14:10:50 -0400

    Added the intent into the post request

--- src/webhook.py
+++ src/webhook.py
@@ -2,9 +2,14 @@ import httpx
 import os
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request
+from src.intent import extract_intent
 
 load_dotenv()
 LINQ_API_KEY = os.getenv("LINQ_API_KEY")
+
+if not LINQ_API_KEY:
+    raise RuntimeError("LINQ_API_KEY not found in .env")
+
 
 app = FastAPI()
 
@@ -13,6 +18,9 @@ async def receive_message(request: Request):
     body = await request.json()
     print("PAYLOAD:", body)
 
+    # Echo it back to yourself for now
+
+
     # Stop the loop 
     if body["data"]["is_from_me"]:
         return {"status": "ok"}
@@ -20,6 +28,16 @@ async def receive_message(request: Request):
     message_text = body["data"]["message"]["parts"][0]["value"]
     print(f"Message from user: {message_text}")
     chat_id = body["data"]["chat_id"]
+
+    try: 
+        intent = extract_intent(message_text)
+        print("INTENT:", intent)
+        reply = f"I understood: {intent}"
+    except RuntimeError: 
+        reply = "Sorry I could not understand. Try somehting like: 'Follow up with Juan, moving to the next round'"
+
+
+
 
     if not chat_id:
         print("chatId not found in payload")
@@ -32,12 +50,12 @@ async def receive_message(request: Request):
             "parts": [
                 {
                     "type": "text",
-                    "value": "Hello back!"
+                    "value": reply
                 }
             ]
         }
     }
-    
+
     headers = {
         "Authorization": f"Bearer {LINQ_API_KEY}",
         "Content-Type": "application/json"
