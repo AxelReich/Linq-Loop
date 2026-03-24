0052d2fa92e6abc01ab7b8158fe5a6c8318628fe
Axel <axreich@gmail.com>
2026-03-23 21:14:46 -0400

    Organized models so that they are in one folder

--- src/models.py
+++ src/models.py
@@ -5,9 +5,13 @@ class Intent(BaseModel):
     action: str
     notes: str
 
-
 class Meeting(BaseModel):
     summary: str         # In the future we can make it a sales rep, so we can read the content of fireflies ai and send a follow up 
-    start_time: str
+    start_time: str     
     attendee_name: str
     attendee_email: str
+
+class DraftEmail(BaseModel):
+    to: str          # juan@company.com (from calendar attendee)
+    subject: str     # "Following up - Next Steps at [Company]"
+    body: str        # the full email body    
